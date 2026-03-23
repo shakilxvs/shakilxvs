3a68ec5a63337faf6f0d59bcbdead8aef7302d39
Shakil <170756548+shakilxvs@users.noreply.github.com>
2026-03-23 08:51:37 +0600

    Refactor remotePatterns in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,33 +2,14 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        pathname: '/**',
-      },
+      { protocol: 'https', hostname: 'res.cloudinary.com',          pathname: '/**' },
+      { protocol: 'https', hostname: 'firebasestorage.googleapis.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com',   pathname: '/**' },
+      { protocol: 'https', hostname: 'images.unsplash.com',         pathname: '/**' },
+      { protocol: 'https', hostname: 'picsum.photos',               pathname: '/**' },
+      { protocol: 'https', hostname: 'logo.clearbit.com',           pathname: '/**' },
+      { protocol: 'https', hostname: 'cdn.simpleicons.org',         pathname: '/**' },
     ],
   },
 };
-
 module.exports = nextConfig;
