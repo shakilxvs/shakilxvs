af0eba1e2be04b0684769b4b2ccc8d944b33ee50
Shakil <170756548+shakilxvs@users.noreply.github.com>
2026-03-27 02:37:39 +0600

    Update robots.js

--- app/robots.js
+++ app/robots.js
@@ -1,7 +1,7 @@
 export default function robots() {
   return {
     rules: [
-      { userAgent: '*', allow: '/', disallow: '/admin/' },
+      { userAgent: '*', allow: '/', disallow: ['/admin/', '/portal/'] },
     ],
     sitemap: 'https://shakilxvs.com/sitemap.xml',
   };
